6185a463940a527b34c4bfaf54d0a01c4c771de0
ADT109119 <106337749+ADT109119@users.noreply.github.com>
2022-05-27 00:42:13 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1653583203,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/bird-sheet0.png",
+		"images/pipebottom-sheet0.png",
+		"images/floor1.png",
+		"images/floor2.png",
+		"images/tiledbackground.png",
+		"images/checkpoint-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
